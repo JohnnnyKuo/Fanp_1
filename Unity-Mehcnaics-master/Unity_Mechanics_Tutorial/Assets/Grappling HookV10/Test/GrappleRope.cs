ee42110687b6ca49a6e4a9813b7e2b19234ca0ae
Johnny.K <a0962001927@gmail.com>
2023-10-19 22:58:15 +0800

    V1

---
+++ Unity-Mehcnaics-master/Unity_Mechanics_Tutorial/Assets/Grappling HookV10/Test/GrappleRope.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+
+public class GrappleRope : MonoBehaviour
+{
+     [Header("General refrences:")]
+     public GrapplingGun grapplingGun; // 引用到 GrapplingGun 腳本的實例，用於取得資訊和呼叫功能
+     [SerializeField] LineRenderer m_lineRenderer; // 引用到 LineRenderer 元件，用於繪製繩索
+
+     [Header("General Settings:")]
+     [SerializeField] private int percision = 20; // 繩索點的數量，用於繪製繩索的精度
+     [Range(0, 100)][SerializeField] private float straightenLineSpeed = 4; // 繩子拉直的速度
+
+     [Header("Animation:")]
+     public AnimationCurve ropeAnimationCurve; // 用於繪製繩索動畫的曲線
+     [SerializeField] [Range(0.01f, 4)] private float WaveSize = 20; // 繩索波動的大小
+     float waveSize;
+
+     [Header("Rope Speed:")]
+     public AnimationCurve ropeLaunchSpeedCurve; // 用於繩索發射速度的曲線
+     [SerializeField] [Range(1, 50)] private float ropeLaunchSpeedMultiplayer = 4; // 繩索發射速度的倍數
+
+     float moveTime = 0; // 繩索移動的時間
+
+     [SerializeField] public bool isGrappling = false; // 指示是否正在使用繩索
+
+     bool drawLine = true; // 指示是否需要繪製繩線
+     bool straightLine = true; // 指示繩子是否為直線狀態
+
+     private void Awake()
+     {
+         m_lineRenderer = GetComponent<LineRenderer>(); // 取得 LineRenderer 元件的引用
+         m_lineRenderer.enabled = false; // 停用 LineRenderer 元件
+         m_lineRenderer.positionCount = percision; // 設定繩索點的數量
+         waveSize = WaveSize; // 設定初始波動大小
+     }
+
+     private void OnEnable()
+     {
+         moveTime = 0; // 重置繩索運動時間
+         m_lineRenderer.enabled = true; // 啟用 LineRenderer 元件
+         m_lineRenderer.positionCount = percision; // 設定繩索點的數量
+         waveSize = WaveSize; // 設定波動大小
+         straightLine = false; // 將繩索狀態設為非直線狀態
+         LinePointToFirePoint(); // 將繩索的起點設定為發射點
+     }
+
+     private void OnDisable()
+     {
+         m_lineRenderer.enabled = false; // 停用 LineRenderer 元件
+         isGrappling = false; // 指示不再使用繩索
+     }
+
+     void LinePointToFirePoint()
+     {
+         for (int i = 0; i < percision; i++)
+         {
+             m_lineRenderer.SetPosition(i, grapplingGun.firePoint.position); // 將繩子的所有點位置設定為發射點的位置
+         }
+     }
+
+     void Update()
+     {
+         moveTime += Time.deltaTime; // 更新繩索運動時間
+
+         if (drawLine)
+         {
+             DrawRope(); // 繪製繩索
+         }
+     }
+
+     void DrawRope()
+     {
+         if (!straightLine)
+         {
+             if (m_lineRenderer.GetPosition(percision - 1).x != grapplingGun.grapplePoint.x)
+             {
+                 DrawRopeWaves(); // 繪製帶有波動的繩索
+             }
+             else
+             {
+                 straightLine = true; // 如果繩子已經拉直，將繩索狀態設為直線狀態
+             }
+         }
+         else
+         {
+             if (!isGrappling)
+             {
+                 grapplingGun.Grapple(); // 呼叫 GrapplingGun 的 Grapple 方法，開始使用繩索
+                 isGrappling = true; // 指示正在使用繩索
+             }
+             if (waveSize > 0)
+             {
+                 waveSize -= Time.deltaTime * straightenLineSpeed; // 縮小波動大小，使繩索逐漸變直
+                 DrawRopeWaves(); // 繪製帶有波動的繩索
+             }
+             else
+             {
+                 waveSize = 0; // 波動大小為零，繩子變直
+                 DrawRopeNoWaves(); // 繪製無波動的繩索
+             }
+         }
+     }
+
+     void DrawRopeWaves()
+     {
+         for (int i = 0; i < percision; i++)
+         {
+             float delta = (float)i / ((float)percision - 1f); // 計算插值參數
+             Vector2 offset = Vector2.Perpendicular(grapplingGun.DistanceVector).normalized * ropeAnimationCurve.Evaluate(delta) * waveSize; // 計算波浪偏移量
+             Vector2 targetPosition = Vector2.Lerp(grapplingGun.firePoint.position, grapplingGun.grapplePoint, delta) + offset; // 計算目標位置
+             Vector2 currentPosition = Vector2.Lerp(grapplingGun.firePoint.position, targetPosition, ropeLaunchSpeedCurve.Evaluate(moveTime) * ropeLaunchSpeedMultiplayer); // 計算目前位置
+
+             m_lineRenderer.SetPosition(i, currentPosition); // 設定繩索點的位置
+         }
+     }
+
+     void DrawRopeNoWaves()
+     {
+         m_lineRenderer.positionCount = 2; // 只設定兩點來繪製直線
+         m_lineRenderer.SetPosition(0, grapplingGun.grapplePoint); // 設定繩子的終點位置
+         m_lineRenderer.SetPosition(1, grapplingGun.firePoint.position); // 設定繩子的起點位置
+     }
+}
---
+++ Unity-Mehcnaics-master/Unity_Mechanics_Tutorial/Assets/Grappling HookV10/Test/GrapplingGun.cs
@@ -0,0 +1,177 @@
+﻿using UnityEngine;
+
+public class GrapplingGun : MonoBehaviour
+{
+    [Header("Scripts:")]
+    public GrappleRope grappleRope; // 引用另一個腳本的變數，用於處理繩索的功能。
+    
+    [Header("Layer Settings:")]
+    [SerializeField] private bool grappleToAll = false; // 是否可以抓取所有物件的布林值。
+    [SerializeField] private int grappableLayerNumber = 9; // 可抓取的物件所在的圖層編號。
+
+    [Header("Main Camera")]
+    public Camera m_camera; // 主攝影機的引用。
+
+    [Header("Transform References:")]
+    public Transform gunHolder; // 抓取槍的持有者的位置。
+    public Transform gunPivot; // 抓取槍的底座的位置。
+    public Transform firePoint; // 抓取槍的發射點的位置。
+
+    [Header("Rotation:")]
+    [SerializeField] private bool rotateOverTime = true; // 是否允許旋轉，並且是否以時間進行平滑旋轉。
+    [Range(0, 80)] [SerializeField] private float rotationSpeed = 4; // 旋轉速度的範圍。
+
+    [Header("Distance:")]
+    [SerializeField] private bool hasMaxDistance = true; // 是否有最大距離的布林值。
+    [SerializeField] private float maxDistance = 4; // 最大抓取距離。
+
+    [Header("Launching")]
+    [SerializeField] private bool launchToPoint = true; // 是否將角色發射到抓取點的布林值。
+    [SerializeField] private LaunchType Launch_Type = LaunchType.Transform_Launch; // 發射的方式，可能是Transform或Physics方式。
+    [Range(0, 5)] [SerializeField] private float launchSpeed = 5; // 發射速度。
+
+    [Header("No Launch To Point")]
+    [SerializeField] private bool autoCongifureDistance = false; // 是否自動配置距離的布林值。
+    [SerializeField] private float targetDistance = 3; // 目標距離。
+    [SerializeField] private float targetFrequency = 3; // 目標頻率。
+
+    // 定義了一個列舉型別，表示發射的方式。
+    private enum LaunchType
+    {
+        Transform_Launch,
+        Physics_Launch,
+    }
+
+    [Header("Component References:")]
+    public SpringJoint2D m_springJoint2D; // 彈簧關節的引用。
+
+    [HideInInspector] public Vector2 grapplePoint; // 抓取點的位置。
+    [HideInInspector] public Vector2 DistanceVector; // 距離向量。
+    Vector2 Mouse_FirePoint_DistanceVector; // 滑鼠位置和發射點之間的距離向量。
+
+    public Rigidbody2D ballRigidbody; // 球的剛體。
+
+    // 在物件啟動時執行的函數。
+    private void Start()
+    {
+        grappleRope.enabled = false; // 關閉繩索功能。
+        m_springJoint2D.enabled = false; // 關閉彈簧關節功能。
+        ballRigidbody.gravityScale = 1; // 設定球的重力。
+    }
+
+    // 在每一幀更新時執行的函數。
+    private void Update()
+    {
+        // 計算滑鼠位置和發射點之間的距離向量。
+        Mouse_FirePoint_DistanceVector = m_camera.ScreenToWorldPoint(Input.mousePosition) - gunPivot.position;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            SetGrapplePoint(); // 設定抓取點。
+        }
+        else if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (grappleRope.enabled)
+            {
+                RotateGun(grapplePoint, false); // 如果正在使用繩索，則旋轉抓取槍。
+            }
+            else
+            {
+                RotateGun(m_camera.ScreenToWorldPoint(Input.mousePosition), false); // 否則，根據滑鼠位置旋轉抓取槍。
+            }
+
+            if (launchToPoint && grappleRope.isGrappling)
+            {
+                if (Launch_Type == LaunchType.Transform_Launch)
+                {
+                    gunHolder.position = Vector3.Lerp(gunHolder.position, grapplePoint, Time.deltaTime * launchSpeed);
+                }
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            grappleRope.enabled = false; // 關閉繩索功能。
+            m_springJoint2D.enabled = false; // 關閉彈簧關節功能。
+            ballRigidbody.gravityScale = 1; // 恢復球的重力。
+        }
+        else
+        {
+            RotateGun(m_camera.ScreenToWorldPoint(Input.mousePosition), true); // 如果沒有按下滑鼠按鈕，則根據滑鼠位置旋轉抓取槍。
+        }
+    }
+
+    // 用於旋轉抓取槍的函數。
+    void RotateGun(Vector3 lookPoint, bool allowRotationOverTime)
+    {
+        Vector3 distanceVector = lookPoint - gunPivot.position;
+        float angle = Mathf.Atan2(distanceVector.y, distanceVector.x) * Mathf.Rad2Deg;
+        if (rotateOverTime && allowRotationOverTime)
+        {
+            Quaternion startRotation = gunPivot.rotation;
+            gunPivot.rotation = Quaternion.Lerp(startRotation, Quaternion.AngleAxis(angle, Vector3.forward), Time.deltaTime * rotationSpeed);
+        }
+        else
+            gunPivot.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    // 設定抓取點的函數。
+    void SetGrapplePoint()
+    {
+        if (Physics2D.Raycast(firePoint.position, Mouse_FirePoint_DistanceVector.normalized))
+        {
+            RaycastHit2D _hit = Physics2D.Raycast(firePoint.position, Mouse_FirePoint_DistanceVector.normalized);
+            if ((_hit.transform.gameObject.layer == grappableLayerNumber || grappleToAll) && ((Vector2.Distance(_hit.point, firePoint.position) <= maxDistance) || !hasMaxDistance))
+            {
+                grapplePoint = _hit.point;
+                DistanceVector = grapplePoint - (Vector2)gunPivot.position;
+                grappleRope.enabled = true; // 啟用繩索功能。
+            }
+        }
+    }
+
+    // 執行抓取操作的函數。
+    public void Grapple()
+    {
+        if (!launchToPoint && !autoCongifureDistance)
+        {
+            m_springJoint2D.distance = targetDistance;
+            m_springJoint2D.frequency = targetFrequency;
+        }
+
+        if (!launchToPoint)
+        {
+            if (autoCongifureDistance)
+            {
+                m_springJoint2D.autoConfigureDistance = true;
+                m_springJoint2D.frequency = 0;
+            }
+            m_springJoint2D.connectedAnchor = grapplePoint;
+            m_springJoint2D.enabled = true;
+        }
+        else
+        {
+            if (Launch_Type == LaunchType.Transform_Launch)
+            {
+                ballRigidbody.gravityScale = 0;
+                ballRigidbody.velocity = Vector2.zero;
+            }
+            if (Launch_Type == LaunchType.Physics_Launch)
+            {
+                m_springJoint2D.connectedAnchor = grapplePoint;
+                m_springJoint2D.distance = 0;
+                m_springJoint2D.frequency = launchSpeed;
+                m_springJoint2D.enabled = true;
+            }
+        }
+    }
+
+    // 在Scene視圖中繪製Gizmos的函數，以可視化最大抓取距離。
+    private void OnDrawGizmos()
+    {
+        if (hasMaxDistance)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(firePoint.position, maxDistance);
+        }
+    }
+}
