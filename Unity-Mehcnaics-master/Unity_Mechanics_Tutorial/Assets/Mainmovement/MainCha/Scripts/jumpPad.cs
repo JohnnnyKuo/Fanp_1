8a9e4346126604549bed99b71eb73d8beb2eb8d9
Johnny.K <a0962001927@gmail.com>
2024-02-27 14:16:38 +0800

    227
    Add jump pad

--- Unity-Mehcnaics-master/Unity_Mechanics_Tutorial/Assets/Mainmovement/MainCha/Scripts/jumpPad.cs
+++ Unity-Mehcnaics-master/Unity_Mechanics_Tutorial/Assets/Mainmovement/MainCha/Scripts/jumpPad.cs
@@ -1,16 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine.Utility;
 using UnityEngine;
 
 public class jumpPad : MonoBehaviour
 {
-    private float bounce = 20f;
+     public float forceMagnitude = 100f; // Adjust this value to control the force strength
+    public float smoothTime = 0.05f; // Adjust this value to control the smoothness of force application
+
+    private Vector2 currentVelocity = Vector2.up;
+
+    private float bounce = 300f;
+    public float dampingRatio = 0.5f;
+    public float offset = 5;
+    public bool spaceDown =  false;
+
     
-    private void OnCollisionEnter2D(Collision2D collision)
+    void FixedUpdate()
+    {   // }
+        spaceDown = Input.GetKey ( KeyCode.Space );
+     
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+        if (other.gameObject.CompareTag("padAffect") && spaceDown)
+        {            
+          
+                 Vector2 forceDirection = Vector2.up;
+            
+             // Smoothly apply force using Vector2.SmoothDamp
+                Vector2 smoothForce = Vector2.SmoothDamp(other.GetComponent<Rigidbody2D>().velocity, forceDirection * forceMagnitude, ref currentVelocity,Time.fixedDeltaTime);
+                Debug.Log(smoothForce);
+
+             // Apply the force to the Rigidbody2D
+           
+                other.GetComponent<Rigidbody2D>().AddForce(smoothForce, ForceMode2D.Impulse);
+            
         }   
+        
     }
 }
